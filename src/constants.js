951ce02f0243d1ed4516953a1c376ec0e89a8ee6
Tejinder Singh <dev.singhtejinder@gmail.com>
2020-11-21 22:42:44 -0500

    till stepp 4

--- src/constants.js
+++ src/constants.js
@@ -37,6 +37,15 @@ let totalTime = {
 	X7: 0,
 };
 
+// let totalTime = {
+// 	X1: [0, 0],
+// 	X2: [0, 0],
+// 	X3: [0, 0],
+// 	X4: [0, 0],
+// 	X5: [0, 0],
+// 	X6: [0, 0],
+// 	X7: [0, 0],
+// };
 export {
 	SCHEDULE_TABLE_HEADER,
 	LOAD_TABLE_HEADER,
